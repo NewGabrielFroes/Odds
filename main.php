1ea30b94185a192a3fa18dfb3568a7c1bb42c24d
NewGabrielFroes <gabriel.froes32@gmail.com>
2022-04-12 18:23:40 -0300

    Retirando ifs

--- main.php
+++ main.php
@@ -7,12 +7,7 @@ $pdo = connect();
 $field_type = "Betano";
 $url = "https://br.betano.com/adserve?type=OddsComparisonFeed&lang=pt&sport=FOOT";
 
-if($field_type == "Betano") {
-	$platform_type = new Betano($pdo, $field_type);
-}
-/*else if($field_type == "Placard") {
-	$platform_type = new Placard($pdo, $field_type);
-}*/
 
+$platform_type = new Betano($pdo, $field_type);
 $json_decoded = $platform_type->convert_json_to_object($url);
 $platform_type->run_through_json($json_decoded);
